f3d23be495015a8561345ec44710072cc0a743ac
Vyshnavi1410 <vyshnavikattamuri14@gmail.com>
2025-04-17 11:15:39 -0400

    TaskOne

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
